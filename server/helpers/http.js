7c098cc2ccbd47f22a90032bf38aededae455e27
Evgenii Vorobey <evgeniivorobey1404@gmail.com>
2019-02-10 15:31:18 +0200

    upds

--- server/helpers/http.js
+++ server/helpers/http.js
@@ -1,11 +1,9 @@
 export const normalizePort = port => {
     const resultPort = parseInt(port, 10);
     if (Number.isNaN(port)) {
-        // named pipe
         return port;
     }
     if (port >= 0) {
-        // port number
         return resultPort;
     }
     return false;
